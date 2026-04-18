2341067e14f26db0df42f309b503d174c2db984b
madhavareddyrb <madhavareddyrb@gmail.com>
2026-04-18 09:54:50 +0530

    Authorization Fixed from FE to BE

--- LMS-PROJECT-FRONTEND/src/Components/UserProfileComponents/UserDashboard/UserDashboard.jsx
+++ LMS-PROJECT-FRONTEND/src/Components/UserProfileComponents/UserDashboard/UserDashboard.jsx
@@ -1,38 +1,82 @@
-import Navbar from "../../HomePageComponents/Navbar/Navbar";
-import axios from "axios";
-import { useEffect, useState } from "react";
+// import Navbar from "../../HomePageComponents/Navbar/Navbar";
+// import axios from "axios";
+// import { useEffect, useState } from "react";
 
-export default function UserDashboard() {
-  const [getUserData, setGetUserData] = useState([]);
+// export default function UserDashboard() {
+//   const [getUserData, setGetUserData] = useState([]);
+//   const access_token = localStorage.getItem("access_token");
+
+//   async function getUserData () {
+//     const token = localStorage.getItem("access_token");
+//     console.log(token);
+//     if (!token) return;
+
+//     await axios.get("http://localhost:3000/userprofile", {
+//       headers: {
+//         Authorization: `Bearer ${token}`,
+//       },
+//     });
+//   };
+
+//   return (
+//     <>
+//       <Navbar />
+
+//       <h2>User Profile Coming Soon</h2>
+//     </>
+//   );
+// }
+
+import Navbar from "@/Components/HomePageComponents/Navbar/Navbar";
+import axios from "axios";
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+
+export default function Orders() {
+  const [ordersData, setOrdersData] = useState([]);
+  const access_token = localStorage.getItem("access_token");
+  console.log(access_token);
+
+  async function getOrders() {
+    try {
+      const token = localStorage.getItem("access_token");
+
+      const response = await axios.get("http://localhost:3000/userprofile", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      console.log("FULL RESPONSE:", response);
+      console.log("DATA:", response.data);
+
+      setOrdersData(response.data.results);
+    } catch (error) {
+      console.log("ERROR:", error.response?.data || error.message);
+    }
+  }
 
   useEffect(() => {
-    const fetchUserData = async () => {
-      try {
-        const response = await axios.get("http://localhost:3000/userprofile");
-        setGetUserData(response.data);
-      } catch (error) {
-        console.log(error.message);
-      }
-    };
-  });
-
-  async () => {
-    const token = localStorage.getItem("access_token");
-    console.log(token);
-    if (!token) return;
-
-    await axios.get("http://localhost:3000/user/profile", {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-  };
+    getOrders();
+  }, []);
 
   return (
-    <>
-      <Navbar />
 
-      <h2>User Profile Coming Soon</h2>
-    </>
+    <div>
+      <Navbar/>
+      Orders
+      {ordersData &&
+        ordersData.map((ele) => {
+          return (
+            <>
+              Hello orders
+              {ele.userName}
+              {/* {ele.fullname} */}
+              {ele.email}
+            </>
+          );
+        })}
+    </div>
   );
 }
